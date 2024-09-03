3015d502514aacecff6af3c8c57f6068f2d8a2cd
shorif <sitarek77@gmail.com>
2024-09-03 10:36:11 +0600

    morning

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Colors from "./Colors/Colors";
 
 const Sidebar = () => {
   return (
-    <section className=" fixed overflow-y-scrolls invisible hidden md:flex  md:flex-col justify-center items-center gap-5 md:pl-10   lg:pl-16   h-full ">
+    <section className=" fixed overflow-y-scrolls  md:flex  md:flex-col justify-center items-center gap-5 md:pl-10   lg:pl-16   h-full ">
       <div className="">
         <h1 className="border-b-2 w-full text-4xl pt-10">🛒</h1>
       </div>
